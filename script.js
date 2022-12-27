1e08d5dd7a0914953ac149b2034e99457c76a0d6
mhickner44 <rmicu1903@gmail.com>
2022-12-27 09:12:56 -0500

    gui added

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+let userInput = null;
+let results = null;
+let userScore = 0;
+let compScore = 0;
+let draws=0;
+
+///event listeneers
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector('.scissors');
+
+const resultsDiv = document.querySelector('.results');
+const wins = document.querySelector('.userScore');
+const losses = document.querySelector('.compScore');
+const final = document.querySelector('.final');
+const drawScore = document.querySelector('.drawScore');
 
 
 
@@ -8,13 +24,28 @@ function getComputerGuess() {
 }
 
 
-function getHumanGuess() {
+function setHumanGuess(userThrow) {
 
     //you have to input numbers 0 1 2  for now until the GUI version
-    let userInput = prompt('Enter "scissors" "Rock" or "Paper"');
-    return userInput;
+    //take the input from the button press 
+    //0 scissors 1 rock 2 paper
+  
+
+    // if(this.id){}
+    if (userThrow == "rock") {
+        userInput = 1;
+    } else if (userThrow == "paper") {
+        userInput = 2;
+
+    } else {
+        userInput = 0;
+    }
+
+
 
 }
+
+
 
 
 function playRound(userThrow, computerThrow) {
@@ -23,14 +54,17 @@ function playRound(userThrow, computerThrow) {
     let winner;
     let userWin = "You won";
     let compWin = "The computer won";
+ 
     console.log(computerThrow);
-   
+    console.log(userThrow);
+
+
     //decides if it was a draw 
     if (userThrow == computerThrow) {
         winner = "draw"
         return winner;
     }
-   
+
     //ROCK
     if (userThrow == 0) {
         if (userThrow == 0 && computerThrow == 2) {
@@ -66,40 +100,44 @@ function playRound(userThrow, computerThrow) {
 
 function game() {
     let gamesPlayed = 0;
-    let userScore = 0;
-    let compScore = 0;
+  
+    setHumanGuess(this.classList.value);
 
-    for (let i = 0; i <= 4; i++) {
-        result = playRound(getHumanGuess(), getComputerGuess());
-        if (result == "draw") {
-            // deducts a game so it plays 5 times if there are draws
-            i--;
+    result = playRound(userInput, getComputerGuess());
+    if (result == "draw") {
+        // deducts a game so it plays 5 times if there are draws
+        draws++;
+       drawScore.textContent = "Draws "+ draws;
+    } else {
+        if (result == "You won") {
+            userScore++;
+            resultsDiv.textContent = "You won"
+            wins.textContent = "Wins " + userScore;
         } else {
-            if (result == "You won") {
-                userScore++;
-            } else {
-                compScore++;
-
-            }
+            compScore++;
+            result = "The computer won";
+            losses.textContent = "Losses " + compScore;
 
         }
-        gamesPlayed++;
+
     }
-//calculate winner
-if(userScore>compScore){
-    result= "You won " 
+   gamesPlayed++;
+   
+    //string for the output
+    if((userScore || compScore )== 5){
+        final.textContent="GAME OVER"
 
-}else{
-    result="The computer won"
-}
+    }
 
-//string for the output
-    result = result + " and you played " + gamesPlayed + " games." + "You won " + userScore + " of them." +
-        "The computer won " + compScore + " of them.";
 
-    return result;
+
+
 }
 ///////////
 
 
-console.log(game());
+
+
+rock.addEventListener('click', game);
+paper.addEventListener('click', game);
+scissors.addEventListener('click', game);
